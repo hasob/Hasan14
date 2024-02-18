d6632c30311f3da19d8fafc032417c20640542a7
hasob <110573422+hasob@users.noreply.github.com>
2024-02-19 02:39:15 +0300

    Add files via upload

---
+++ mody.py
@@ -0,0 +1,19 @@
+import os
+
+
+class Mody(object):
+    ELHYBA = os.environ.get("ELHYBA", "")
+    
+    OWNER = os.environ.get("OWNER", "6798245975")
+    
+    ID_BOT = int(os.environ.get("ID_BOT", "6798245975"))
+    
+    USER_BOT = os.environ.get("USER_BOT", "Gkyjjbvvbot")
+    
+    MAX_ACCOUNTS = int(os.environ.get("MAX_ACCOUNTS", "500"))
+    
+    API_ID = os.environ.get("API_ID", "26004411")
+    
+    API_HASH = os.environ.get("API_HASH", "4b9a73c02e89da49ef7f7df8a7e30f65")
+    
+
